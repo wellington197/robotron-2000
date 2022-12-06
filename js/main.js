263697cf153f7e9f1d12820360a2e7fc8e5700f1
wellington_197 <fcowellcosta@gmail.com>
2022-12-06 13:36:48 -0300

    Alterando elementos

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const braco = document.querySelecto("#braco")
+
+const controle = document.querySelectorAll(".controle-ajuste")
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+    })
+})
+
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector("#controle-contador")
+    if(operacao === "-") {
+        peca.value = parseInt(braco.value) - 1
+    } else { 
+        peca.value = parseInt(braco.value) + 1
+    }
+}
